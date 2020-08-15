23487f906d2e43d626d08c8e11c1831fa9b21222
Gabriel Rodrigues <rsgbariel01@gmail.com>
2020-08-15 19:44:11 -0300

    criado relacionamentos da entidade Neighborhoods

--- backend/src/app/models/d-Neighborhoods.js
+++ backend/src/app/models/d-Neighborhoods.js
@@ -13,5 +13,29 @@ module.exports = (sequelize, DataTypes) => {
       timestamps: false,
     }
   );
+
+  Neighborhoods.associate = (models) => {
+    Neighborhoods.hasOne(models.Travel_fee, {
+      foreignKey: "id_neighborhood",
+      as: "Neighborhoods-Travel_fee",
+    });
+    Neighborhoods.hasMany(models.Service_orders, {
+      foreignKey: "id_neighborhood_origin",
+      as: "Neighborhoods-neighborhood_origin-Service_orders",
+    });
+    Neighborhoods.hasMany(models.Service_orders, {
+      foreignKey: "id_neighborhood_destiny",
+      as: "Neighborhoods-neighborhood_destiny-Service_orders",
+    });
+    Neighborhoods.hasMany(models.People_address, {
+      foreignKey: "id_neighborhood",
+      as: "Neighborhoods-People_address",
+    });
+    Neighborhoods.belongsTo(models.Citys, {
+      foreignKey: "id",
+      as: "Neighborhoods-Citys",
+    });
+  };
+
   return Neighborhoods;
 };

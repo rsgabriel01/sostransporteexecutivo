0c1250c99ab8fd1ef5921fdc087515c4f81eb51f
Gabriel Rodrigues <rsgbariel01@gmail.com>
2020-08-17 01:44:53 -0300

    queries auxiliares

---
+++ backend/src/database/scripts/queriesAuxiliaries.sql
@@ -0,0 +1,19 @@
+SELECT * FROM "People"
+
+SELECT * FROM "People_address"
+
+SELECT * FROM "Users"
+
+SELECT * FROM "Sessions"
+
+SELECT * FROM "Citys"
+
+SELECT * FROM "Neighborhoods"
+
+SELECT * FROM "Types"
+
+SELECT * FROM "Type_people"
+
+SELECT * FROM "Citys" WHERE name = 'CASCAVEL'
+
+SELECT * FROM "Neighborhoods" WHERE id_city = 5941

34e2744deb47428c2adf83db8a6f9433f74091d9
Gabriel Rodrigues <rsgabriel01@gmail.com>
2020-09-21 21:52:40 -0300

    queries auxiliares

--- backend/src/database/scripts/queriesAuxiliaries.sql
+++ backend/src/database/scripts/queriesAuxiliaries.sql
@@ -22,6 +22,22 @@ INSERT INTO "People" (name, active) VALUES ('ADMINISTRADOR', true);
 
 INSERT INTO "People" (name, cpf_cnpj, rg, phone, email, active) VALUES ('GABRIEL RODRIGUES SOUZA', '092997520901', '128930590', '45998044717', 'rsgabriel01@gmail.com', true);
 
-INSERT INTO "Type_people" (id_people, id_type) VALUES (1, 1);
+INSERT INTO "People" (name, cpf_cnpj, rg, phone, email, active) VALUES ('ELAINE DE OLIVEIRA ZANINI', '74136147074', '128930591', '45999887766', 'ezanini1@fag.edu.br', true);
 
-INSERT INTO "Type_people" (id_people, id_type) VALUES (2, 1);
+INSERT INTO "People" (name, cpf_cnpj, rg, phone, email, active) VALUES ('ANDRÉ AGUSTO RIBEIRO HELENA', '41865677000', '128930592', '45988776655', 'andrehelena@fag.edu.br', true);
+
+INSERT INTO "Type_people" (id_people, id_type, active) VALUES (1, 1, true);
+
+INSERT INTO "Type_people" (id_people, id_type, active) VALUES (1, 2, false);
+
+INSERT INTO "Type_people" (id_people, id_type, active) VALUES (2, 1, true);
+
+INSERT INTO "Type_people" (id_people, id_type, active) VALUES (2, 2, false);
+
+INSERT INTO "Type_people" (id_people, id_type, active) VALUES (3, 1, true);
+
+INSERT INTO "Type_people" (id_people, id_type, active) VALUES (3, 2, false);
+
+INSERT INTO "Type_people" (id_people, id_type, active) VALUES (4, 1, true);
+
+INSERT INTO "Type_people" (id_people, id_type, active) VALUES (4, 2, false);
